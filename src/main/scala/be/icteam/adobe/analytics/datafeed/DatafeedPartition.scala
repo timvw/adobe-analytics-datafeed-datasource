bf464087b338447039d29512854afeb4eeb8022e
Tim Van Wassenhove <tim@timvw.be>
2023-11-08 11:46:37 +0100

    introduce util package

--- src/main/scala/be/icteam/adobe/analytics/datafeed/DatafeedPartition.scala
+++ src/main/scala/be/icteam/adobe/analytics/datafeed/DatafeedPartition.scala
@@ -1,5 +1,6 @@
 package be.icteam.adobe.analytics.datafeed
 
+import be.icteam.adobe.analytics.datafeed.util.{DataFile, ManifestFile}
 import org.apache.spark.sql.connector.read.InputPartition
 
 case class DatafeedPartition(dataFile: DataFile, manifestFile: ManifestFile, options: DatafeedOptions) extends InputPartition
