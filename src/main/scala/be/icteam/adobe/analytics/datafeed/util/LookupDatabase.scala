8ae03c5593923223c39c725de4087a998373c4d5
Tim Van Wassenhove <tim@timvw.be>
2023-11-09 08:29:33 +0100

    rework to reduce number of serde calls

--- src/main/scala/be/icteam/adobe/analytics/datafeed/util/LookupDatabase.scala
+++ src/main/scala/be/icteam/adobe/analytics/datafeed/util/LookupDatabase.scala
@@ -6,16 +6,14 @@ import org.rocksdb.{Options, RocksDB}
 import java.io.{File, FileInputStream}
 import java.nio.file.Files
 
-case class LookupDatabase[T](lookupFile: File, buildLookupValue: Array[String] => T, serialize: T => Array[Byte], deserialize: Array[Byte] => T) extends AutoCloseable {
+case class LookupDatabase(lookupFile: File, buildKeyAndValue: Array[String] => (Array[Byte], Array[Byte])) extends AutoCloseable {
 
-  def get(key: Array[Byte]): T = {
-    val bytes = lookupFileDb.get(key)
-    deserialize(bytes)
+  def get(key: Array[Byte]): Array[Byte] = {
+    lookupFileDb.get(key)
   }
 
-  def set(key: Array[Byte], value: T): Unit = {
-    val bytes = serialize(value)
-    lookupFileDb.put(key, bytes)
+  def set(key: Array[Byte], value: Array[Byte]): Unit = {
+    lookupFileDb.put(key, value)
   }
 
   protected lazy val tsvParser: TsvParser = {
@@ -37,8 +35,7 @@ case class LookupDatabase[T](lookupFile: File, buildLookupValue: Array[String] =
   import scala.collection.JavaConverters._
   val lookupStream = new FileInputStream(lookupFile)
   tsvParser.iterate(lookupStream).iterator().asScala.foreach(x => {
-    val key = x.head.getBytes
-    val value = buildLookupValue(x.tail)
+    val (key, value) = buildKeyAndValue(x)
     set(key, value)
   })
   lookupStream.close()
